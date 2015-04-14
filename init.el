87b98c29975a1ebb3b49b3125c5c1ab0be92f997
Pavel Iosad <pavel.iosad@ed.ac.uk>
2015-04-14 16:38:29 +0100

    Initial commit

---
+++ init.el
@@ -0,0 +1,27 @@
+;;; init.el --- Bootstrap Emacs configuration
+
+;;; Commentary:
+
+;; This file loads Org-mode and then loads the rest of our Emacs
+;; initialization from Emacs Lisp embedded in literate Org-mode files.
+
+;;; Code:
+
+;; Cask manages our package dependencies
+
+(require 'cask "/usr/local/share/emacs/site-lisp/cask.el")
+(cask-initialize)
+
+;; Pallet allows us to use Cask in tandem with package.el
+(require 'pallet)
+
+;; Now load-up org-mode
+
+(require 'ob-tangle)
+
+;; Now actually load the .org file with the configuration
+
+(org-babel-load-file 
+ (expand-file-name "emacs.org" user-emacs-directory))
+
+;; init.el ends
