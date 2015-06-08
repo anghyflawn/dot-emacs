5b7435096798fdc098fe1882d278db1ac6f62449
Pavel Iosad <pavel.iosad@ed.ac.uk>
2015-06-08 11:45:02 +0100

    A few updates as I move to a newer version

--- init.el
+++ init.el
@@ -9,6 +9,13 @@
 
 ;; Cask manages our package dependencies
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'cask "/usr/local/share/emacs/site-lisp/cask.el")
 (cask-initialize)
 
